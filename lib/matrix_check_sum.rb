bd4c5750c72141ab864fe5fe737f9242dde5e2e7
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-16 13:33:35 -0700

    solution complete

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -3,8 +3,24 @@
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: On^2
+# Space complexity: O1
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+  i = 0
+
+  matrix.length.times do |j|
+    sum = 0
+
+    matrix.each do |array|
+      sum += array[i]
+    end
+
+    if sum != matrix[i].reduce(:+)
+      return false
+    end
+
+    i += 1
+  end
+
+  return true
 end
